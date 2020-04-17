93b3e4cf0cbf9016227358f49f06592121c10b96
CheAle14 <thedark1513@gmail.com>
2020-04-17 20:27:28 +0100

    Fail linux

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -45,6 +45,7 @@ The entire casino section has been dropped
 
 #if WINDOWS
         public const string BASE_PATH = @"D:\Bot\";
+        ;;
 #else
         public const string BASE_PATH = @"/mnt/drive/bot/Data/";
 #endif
