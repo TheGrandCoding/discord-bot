ada42368ddb2c7d317f34ec73c3738c3fa2682e6
CheAle14 <thedark1513@gmail.com>
2020-07-14 23:29:25 +0100

    Update CleanSlateProtocol.cs

--- DiscordBot/Services/CleanSlateProtocol.cs
+++ DiscordBot/Services/CleanSlateProtocol.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DiscordBot.Services
 {
-    class CleanSlateProtocol
+    public class CleanSlateProtocol : Service
     {
+        public override void OnDailyTick()
+        {
+            var guild = Program.Client.GetGuild(365230804734967840);
+            var chnl = guild.GetTextChannel(516708276851834926);
+            IMessage last = null;
+            do
+            {
+                IEnumerable<IMessage> messages;
+                if (last == null)
+                    messages = chnl.GetMessagesAsync().FlattenAsync().Result;
+                else
+                    messages = chnl.GetMessagesAsync(last, Direction.Before).FlattenAsync().Result;
+                foreach(var msg in messages)
+                {
+                    if(msg.Author.Id == 133622884122886144)
+                    {
+                        msg.DeleteAsync(new RequestOptions()
+                        {
+                            AuditLogReason = "Clean slate protocol"
+                        });
+                    }
+                    last = msg;
+                }
+            } while (last.CreatedAt.DayOfYear == DateTime.Now.DayOfYear);
+        }
     }
 }
