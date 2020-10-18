af9c3ebc66d34f5fcee4eae08e97fea244bce64e
CheAle14 <thedark1513@gmail.com>
2020-10-18 21:02:30 +0100

    a

---
+++ DiscordBot/Commands/Modules/MonitorModule.cs
@@ -0,0 +1,52 @@
+﻿using Discord.Commands;
+using DiscordBot.Classes;
+using DiscordBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands.Modules
+{
+    [Group("monitor")]
+    public class MonitorModule : BotModule
+    {
+        public MonitorService Service { get; set; }
+        [Command("vc")]
+        public async Task VC(BotUser user)
+        {
+            if(!Service.Monitors.TryGetValue(user.Id, out var monitor))
+            {
+                monitor = new Monitor();
+                Service.Monitors[user.Id] = monitor;
+            }
+            if(monitor.VC.RemoveAll(x => x.Id == Context.User.Id) == 0)
+            {
+                monitor.VC.Add(Context.BotUser.FirstValidUser);
+                await ReplyAsync("Added monitor for user.");
+            } else
+            {
+                await ReplyAsync("Removed monitor for user.")
+            }
+        }
+        [Command("status")]
+        public async Task Status(BotUser user)
+        {
+            if (!Service.Monitors.TryGetValue(user.Id, out var monitor))
+            {
+                monitor = new Monitor();
+                Service.Monitors[user.Id] = monitor;
+            }
+            if (monitor.Status.RemoveAll(x => x.Id == Context.User.Id) == 0)
+            {
+                monitor.Status.Add(Context.BotUser.FirstValidUser);
+                await ReplyAsync("Added monitor for user.");
+            }
+            else
+            {
+                await ReplyAsync("Removed monitor for user.")
+            }
+        }
+    }
+}
---
+++ DiscordBot/Services/MonitorService.cs
@@ -0,0 +1,59 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Services
+{
+    public class MonitorService : SavedService
+    {
+        public Dictionary<ulong, Monitor> Monitors { get; set; }
+        public override string GenerateSave()
+        {
+            return Program.Serialise(Monitors);
+        }
+        public override void OnReady()
+        {
+            Monitors = Program.Deserialise<Dictionary<ulong, Monitor>>(ReadSave());
+            Program.Client.UserVoiceStateUpdated += Client_UserVoiceStateUpdated;
+            Program.Client.GuildMemberUpdated += Client_GuildMemberUpdated;
+        }
+
+        private async System.Threading.Tasks.Task Client_GuildMemberUpdated(SocketGuildUser arg1, SocketGuildUser arg2)
+        {
+            if (!Monitors.TryGetValue((arg1 ?? arg2).Id, out var monitor))
+                return;
+            var builder = new EmbedBuilder();
+            builder.Title = $"Status Updated";
+            builder.WithAuthor(arg1);
+            builder.Description = $"{arg1.Status} to {arg2.Status}";
+            foreach (var usr in monitor.Status)
+                await usr.SendMessageAsync(embed: builder.Build());
+        }
+
+        private async System.Threading.Tasks.Task Client_UserVoiceStateUpdated(Discord.WebSocket.SocketUser arg1, Discord.WebSocket.SocketVoiceState arg2, Discord.WebSocket.SocketVoiceState arg3)
+        {
+            if (!Monitors.TryGetValue(arg1.Id, out var monitor))
+                return;
+            var builder = new EmbedBuilder();
+            builder.Title = $"VC Updated";
+            builder.WithAuthor(arg1);
+            if (arg3.VoiceChannel == null)
+                builder.Description = $"Left {arg2.VoiceChannel.Name}";
+            else if (arg2.VoiceChannel == null)
+                builder.Description = $"Join {arg2.VoiceChannel.Name}";
+            else
+                builder.Description = $"Moved from {arg2.VoiceChannel.Name} to {arg3.VoiceChannel.Name}";
+            foreach (var usr in monitor.VC)
+                await usr.SendMessageAsync(embed: builder.Build());
+        }
+    }
+
+    public class Monitor
+    {
+        public List<SocketGuildUser> VC { get; set; } = new List<SocketGuildUser>();
+        public List<SocketGuildUser> Status { get; set; } = new List<SocketGuildUser>();
+    }
+
+}
