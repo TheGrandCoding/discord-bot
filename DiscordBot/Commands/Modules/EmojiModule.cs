1e31dcc8ebb00a11d428f4efa77c3762e4e230c0
CheAle14 <thedark1513@gmail.com>
2020-11-20 20:01:44 +0000

    Allow role locking emojis

---
+++ DiscordBot/Commands/Modules/EmojiModule.cs
@@ -0,0 +1,64 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands.Modules
+{
+    [Name("Role-locked Emojis")]
+    [RequireContext(ContextType.Guild)]
+    [Group("emoji"), Alias("emote")]
+    public class EmojiModule : BotModule
+    {
+        [Command("list")]
+        [Summary("Lists all role-locked emojis")]
+        public async Task List()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.Title = "Emojis";
+            foreach(var emoji in Context.Guild.Emotes)
+            {
+                if(emoji.RoleIds != null)
+                {
+                    var value = emoji.RoleIds.Select(x => Context.Guild.GetRole(x).Mention);
+                    builder.AddField(emoji.ToString(), string.Join("\r\n", value), true);
+                }
+            }
+            if (builder.Fields.Count == 0)
+                builder.WithDescription($"This server has no emojis that are role-locked." + 
+                    ((Context.User as SocketGuildUser).GuildPermissions.ManageEmojis ? $"\r\nUse `{Program.Prefix}emoji lock` to lock one" : "")
+                    );
+            else
+                builder.WithDescription($"Below are {builder.Fields.Count} emoji that require one of certain roles to use");
+            await ReplyAsync(embed: builder.Build());
+        }
+
+        [Command("lock")]
+        [Summary("Sets an emote to be useable only by a comma-separated list of roles; ")]
+        [RequireUserPermission(GuildPermission.ManageEmojis)]
+        public async Task<RuntimeResult> Lock(GuildEmote emote, [Remainder]string roles = "")
+        {
+            var roleList = new List<IRole>();
+            foreach(var x in roles.Split(','))
+            {
+                var text = x.Trim();
+                if (ulong.TryParse(text, out var id))
+                    roleList.Add(Context.Guild.GetRole(id));
+                else if (MentionUtils.TryParseRole(text, out id))
+                    roleList.Add(Context.Guild.GetRole(id));
+                else
+                    return new BotResult($"Could not parse `{text}` as any role. Either mention it or use the role's id.");
+            }
+            await Context.Guild.ModifyEmoteAsync(emote, x =>
+            {
+                x.Roles = roleList;
+            });
+            await ReplyAsync("Done.");
+            return new BotResult();
+        }
+    }
+}
---
+++ DiscordBot/TypeReaders/GuildEmoteTypeReader.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.TypeReaders
+{
+    public class GuildEmoteTypeReader : BotTypeReader<GuildEmote>
+    {
+        public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            if (context.Guild == null)
+                return TypeReaderResult.FromError(CommandError.UnmetPrecondition, "Must be executed within a guild");
+            if (!Emote.TryParse(input, out var emote))
+                return TypeReaderResult.FromError(CommandError.ParseFailed, $"Could not parse input as an emote");
+            var gem = await context.Guild.GetEmoteAsync(emote.Id);
+            return TypeReaderResult.FromSuccess(gem);
+        }
+    }
+}
