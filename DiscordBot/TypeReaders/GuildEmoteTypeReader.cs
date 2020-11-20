bb6c017c9dbb6dbcb91639e629654c5fe78202a2
CheAle14 <thedark1513@gmail.com>
2020-11-20 20:07:46 +0000

    Update parsing

--- DiscordBot/TypeReaders/GuildEmoteTypeReader.cs
+++ DiscordBot/TypeReaders/GuildEmoteTypeReader.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,10 +14,24 @@ namespace DiscordBot.TypeReaders
         {
             if (context.Guild == null)
                 return TypeReaderResult.FromError(CommandError.UnmetPrecondition, "Must be executed within a guild");
-            if (!Emote.TryParse(input, out var emote))
-                return TypeReaderResult.FromError(CommandError.ParseFailed, $"Could not parse input as an emote");
-            var gem = await context.Guild.GetEmoteAsync(emote.Id);
-            return TypeReaderResult.FromSuccess(gem);
+            if (Emote.TryParse(input, out var emote))
+            {
+                var gem = await context.Guild.GetEmoteAsync(emote.Id);
+                return TypeReaderResult.FromSuccess(gem);
+            }
+            if(ulong.TryParse(input, out var id))
+            {
+                var gem = await context.Guild.GetEmoteAsync(id);
+                return TypeReaderResult.FromSuccess(gem);
+            }
+            if(input.StartsWith(':') == false || (input.StartsWith(':') && input.EndsWith(':')))
+            {
+                var name = input.StartsWith(':') ? input[1..^1] : input;
+                var gem = context.Guild.Emotes.FirstOrDefault(x => x.Name == name);
+                if (gem != null)
+                    return TypeReaderResult.FromSuccess(gem);
+            }
+            return TypeReaderResult.FromError(CommandError.ParseFailed, $"Could not parse input as an emote");
         }
     }
 }
