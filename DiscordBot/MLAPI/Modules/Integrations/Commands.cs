d714c76434d4fbe6f239c1c92982482f6ad93542
CheAle14 <thedark1513@gmail.com>
2021-02-02 11:21:39 +0000

    Update Commands.cs

--- DiscordBot/MLAPI/Modules/Integrations/Commands.cs
+++ DiscordBot/MLAPI/Modules/Integrations/Commands.cs
@@ -68,14 +68,17 @@ namespace DiscordBot.MLAPI.Modules.Integrations
         public async Task Move(IUserMessage message, ITextChannel to)
         {
             var webhook = await Webhooks.GetWebhookClientAsync(to);
+            Program.LogMsg($"Fetched webhook, sending message...");
             await webhook.SendMessageAsync(
                 message.Content,
                 false,
-                message.Embeds.Select(x => x as Embed),
+                message.Embeds == null ? null : message.Embeds.Select(x => x as Embed),
                 message.Author.Username,
                 message.Author.GetAnyAvatarUrl()
                 );
+            Program.LogMsg($"Message sent, deleting message...");
             await message.DeleteAsync();
+            Program.LogMsg($"Moved {message.Id}");
         }
 
         /*public async Task Move(ulong message, ulong channel)
@@ -101,14 +104,18 @@ namespace DiscordBot.MLAPI.Modules.Integrations
         [Id(806096572546023474)]
         public async Task MoveBatch(int amount, ulong channel)
         {
+            Program.LogMsg($"Invoked with {amount} {channel}");
             if(amount > 25)
             {
                 await ErrorAsync("Cannot move that many messages");
                 return;
             }
             var user = Context.User as IGuildUser;
+            Program.LogMsg($"Invoked by {user.Username}");
             var from = Context.Channel as ITextChannel;
+            Program.LogMsg($"Invoked in {from.Name}");
             var to = Program.Client.GetChannel(channel) as ITextChannel;
+            Program.LogMsg($"Moving to {to.Name}");
             var fromPerms = user.GetPermissions(from);
             var toPerms = user.GetPermissions(to);
             if(!(fromPerms.ManageMessages && toPerms.ManageMessages))
@@ -116,8 +123,11 @@ namespace DiscordBot.MLAPI.Modules.Integrations
                 await ErrorAsync("You do not have permission to move messages between those channels");
                 return;
             }
+            Program.LogMsg($"Fetching messages...");
             var _m = await from.GetMessagesAsync((int)amount).FlattenAsync();
+            Program.LogMsg($"Fetched {_m.Count()} messages; sending ACK with source.");
             await AcknowledgeAsync(true);
+            Program.LogMsg($"Starting move of messages...");
             foreach(var msg in _m.OrderBy(x => x.CreatedAt))
             {
                 if(msg is IUserMessage umsg)
