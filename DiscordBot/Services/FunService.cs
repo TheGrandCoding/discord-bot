4131a366dcffb2b19afc4d37c30e6774ab111609
CheAle14 <thedark1513@gmail.com>
2021-03-04 15:42:06 +0000

    Update FunService.cs

--- DiscordBot/Services/FunService.cs
+++ DiscordBot/Services/FunService.cs
@@ -1,4 +1,7 @@
 ﻿using Discord;
+using Discord.WebSocket;
+using DiscordBot.Classes;
+using Markdig.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +15,26 @@ namespace DiscordBot.Services
     {
         public Dictionary<string, List<string>> ImageTriggers { get; set; } = new Dictionary<string, List<string>>();
 
+        Cached<bool> BlockChange = new Cached<bool>(true, 3);
+        static List<int[]> RAINBOW = new List<int[]>()
+            {
+                new int[] {170, 0, 0},
+                new int[] { 255, 85, 85 },
+                new int[] {255,170,0},
+                new int[] {255,255,85},
+                new int[] {0,170,0},
+                new int[] {85,255,85},
+                new int[] {85,255,255},
+                new int[] {0,170,170},
+                new int[] {0,0,170},
+                new int[] {85,85,255},
+                new int[] {255,85,255},
+                new int[] {170,0,170},
+                //new int[] {255,255,255},
+                new int[] {170,170,170},
+                new int[] {85,85,85}
+            };
+
         public override string GenerateSave()
         {
             return Program.Serialise(ImageTriggers);
@@ -20,8 +43,6 @@ namespace DiscordBot.Services
         public override void OnReady()
         {
             Program.Client.MessageReceived += Client_MessageReceived;
-            var th = new Thread(loop);
-            th.Start();
         }
 
         bool TryGetValue(string text, out List<string> s)
@@ -48,56 +69,24 @@ namespace DiscordBot.Services
                 await arg.Channel.SendMessageAsync(possibles[Program.RND.Next(0, possibles.Count)],
                     messageReference: new MessageReference(arg.Id, arg.Channel.Id));
             }
-        }
-    
-        private void loop()
-        {
-            var guild = Program.Client.Guilds.FirstOrDefault(x => x.Name == "Block Land");
-            if (guild == null)
-                return;
-            var role = guild.Roles.FirstOrDefault(x => x.Id == 683065223535788066);
-            if (role == null)
-                return;
-            var rainbow = new List<int[]>()
+            if(arg.Author is SocketGuildUser gUser && arg.Channel is SocketGuildChannel channel)
             {
-                new int[] {170, 0, 0},
-                new int[] { 255, 85, 85 },
-                new int[] {255,170,0},
-                new int[] {255,255,85},
-                new int[] {0,170,0},
-                new int[] {85,255,85},
-                new int[] {85,255,255},
-                new int[] {0,170,170},
-                new int[] {0,0,170},
-                new int[] {85,85,255},
-                new int[] {255,85,255},
-                new int[] {170,0,170},
-                //new int[] {255,255,255},
-                new int[] {170,170,170},
-                new int[] {85,85,85}
-            };
-            int i = 0;
-            do
-            {
-                try
+                var guild = channel.Guild;
+                var role = gUser.Roles.FirstOrDefault(x => x.Name == "Rainbow");
+                if(role != null)
                 {
-                    var t = Program.GetToken();
-                    var usr = guild.GetUser(Program.AppInfo.Owner.Id);
-                    if (usr.Status == UserStatus.Online)
+                    if(BlockChange.GetValueOrDefault() == false)
                     {
-                        var rgb = rainbow[i];
-                        var c = new Color(rgb[0], rgb[1], rgb[2]);
-                        role.ModifyAsync(x => x.Color = c).Wait(t);
-
-                        if (++i >= rainbow.Count)
+                        BlockChange.Value = true;
+                        var color = role.Color;
+                        var i = RAINBOW.IndexOf(new int[] { color.R, color.G, color.B });
+                        if (++i >= RAINBOW.Count)
                             i = 0;
+                        var clr = RAINBOW[i];
+                        await role.ModifyAsync(x => x.Color = new Color(clr[0], clr[1], clr[2]));
                     }
-                    Task.Delay(30000, t).Wait();
-                } catch
-                {
-                    break;
                 }
-            } while (true);
+            }
         }
     }
 }
