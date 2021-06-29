01f890e0b7e4680165abd8eddbf510fdcccff835
The Dark <thedark1513@gmail.com>
2021-06-29 20:56:49 +0100

    bump 0.18.3

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -38,7 +38,7 @@ namespace DiscordBot
 {
     public partial class Program
     {
-        public const string VERSION = "0.18.2"; 
+        public const string VERSION = "0.18.3"; 
         public const string CHANGELOG = VERSION + @"
 == Permissions changes
 Changed how permissions worked for bot.
