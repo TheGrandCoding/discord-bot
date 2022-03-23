f1a7b9308b13d2bb4910ea42f82564aa2ff6a9e4
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2022-03-23 18:43:49 +0000

    test some recipe things

--- DiscordBot/Services/FoodService.cs
+++ DiscordBot/Services/FoodService.cs
@@ -2,13 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace DiscordBot.Services
 {
@@ -63,14 +62,14 @@ namespace DiscordBot.Services
             db.SaveChanges();
             return true;
         }
-        
+
         public string GetManufacturor(string id)
         {
-            foreach((var key, var ls) in Manufacturers)
+            foreach ((var key, var ls) in Manufacturers)
             {
-                foreach(var x in ls)
+                foreach (var x in ls)
                 {
-                    if(x.Length == id.Length)
+                    if (x.Length == id.Length)
                     {
                         if (id.StartsWith(x.Replace("_", "")))
                             return key;
@@ -101,6 +100,7 @@ namespace DiscordBot.Services
         {
             var sv = Program.Deserialise<foodSave>(ReadSave());
             Manufacturers = new ConcurrentDictionary<string, List<string>>(sv.manufacturerPrefixes ?? new Dictionary<string, List<string>>());
+
         }
     }
 
@@ -136,7 +136,7 @@ namespace DiscordBot.Services
         }
         public Product AddProduct(string id, string name, string url, int? extends, string tags)
         {
-            var prod = new Product() { 
+            var prod = new Product() {
                 Id = id,
                 Name = name,
                 Url = url,
@@ -240,7 +240,7 @@ namespace DiscordBot.Services
         public Product Product { get; set; }
         public string ProductId { get; set; }
 
-        public DateTime AddedAt { get; set; }   
+        public DateTime AddedAt { get; set; }
 
         [Column("ExpiresAt")]
         public DateTime InitialExpiresAt { get; set; }
@@ -260,5 +260,230 @@ namespace DiscordBot.Services
 
         public DateTime AddedAt { get; set; }
         public DateTime RemovedAt { get; set; }
+    }
+
+    public class SavedRecipe
+    {
+        public Dictionary<int, int> Ingredients { get; set; }
+        [JsonProperty("steps", ItemTypeNameHandling = TypeNameHandling.All)]
+        public List<SavedStep> Steps { get; set; }
+    }
+
+    public class SavedStep
+    {
+        [JsonProperty("desc")]
+        public string Description { get; set; }
+        [JsonProperty("dura")]
+        public int? Duration { get; set; }
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+        public List<SavedStep> Children { get; set; }
+    }
+
+    public class WorkingRecipe
+    {
+        public List<WorkingMultiStep> Steps { get; set; }
+
+        public WorkingStepBase NextStep { get; set; }
+
+        public WorkingStepBase getNext()
+        {
+            var ordered = Steps.OrderByDescending(x => x.FullLength).ToList();
+            var longestStep = ordered.First();
+            var endsAt = DateTime.Now.AddSeconds(longestStep.FullLength);
+            Console.WriteLine($"Ideal end at: {endsAt}");
+            foreach (var x in Steps) x.UpdateIdealStart(endsAt);
+
+            var ideals = Steps.Where(x => !x.IsFinished).OrderBy(x => x.IdealStartAt);
+            foreach(var step in ideals)
+            {
+                if(step.IsFinished) continue;
+                if (!step.StartedAt.HasValue) return step;
+            }
+            return null;
+
+        }
+
+    }
+
+    public abstract class WorkingStepBase
+    {
+        public virtual string Description { get; protected set; }
+        public virtual int Duration { get; protected set; }
+
+        public virtual int Delay { get; protected set; }
+
+        public virtual DateTime IdealStartAt { get; set; }
+
+        public virtual DateTime? StartedAt { get; protected set; }
+
+        public virtual DateTime? EndsAt
+        {
+            get
+            {
+                if (StartedAt.HasValue)
+                    return StartedAt.Value.AddSeconds(Duration);
+                return null;
+            }
+        }
+
+        public virtual int FullLength
+        {
+            get
+            {
+                if (StartedAt.HasValue)
+                {
+                    var v = (EndsAt.Value - DateTime.Now).TotalSeconds;
+                    if (v > 0)
+                        return (int)Math.Round(v);
+                    return 0;
+                }
+                else if (IsFinished)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return Duration + Delay;
+                }
+            }
+        }
+
+        public virtual DateTime? ActuallyEndedAt { get; private set; }
+
+        public virtual bool IsFinished { get
+            {
+                return ActuallyEndedAt.HasValue;
+            }  set
+            {
+                if (value)
+                    ActuallyEndedAt = DateTime.Now;
+                else
+                    ActuallyEndedAt = null;
+            }
+        }
+
+        public WorkingMultiStep Parent { get; set; }
+
+        public abstract void MarkStarted();
+        public abstract void MarkDone();
+
+        public virtual void UpdateIdealStart(DateTime targetEndsAt)
+        {
+            string c;
+            if (StartedAt.HasValue)
+                c = "*";
+            else if (IsFinished)
+                c = "v";
+            else
+                c = "-";
+            IdealStartAt = targetEndsAt.AddSeconds(FullLength * -1);
+
+            //Console.WriteLine($"   {c} {Description} target {targetEndsAt}, length {FullLength}, start: {IdealStartAt}");
+        }
+        public abstract string GetDebuggerDisplay();
+    }
+
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class WorkingSimpleStep : WorkingStepBase
+    {
+        public WorkingSimpleStep(string desc, int dur, int del)
+        {
+            Description = desc;
+            Duration = dur;
+            Delay = del;
+            StartedAt = null;
+        }
+
+        public override void MarkStarted()
+        {
+            StartedAt = DateTime.Now;
+        }
+
+        public override string GetDebuggerDisplay()
+        {
+            return $"{Description} for {Duration}s taking {Delay}s";
+        }
+
+        public override void MarkDone()
+        {
+            IsFinished = true;
+        }
+    }
+
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class WorkingMultiStep : WorkingStepBase
+    {
+        public string Title { get; set; }
+        public List<WorkingStepBase> Children { get; }
+        public int Step { get; set; }
+
+        public WorkingStepBase Current => Children.ElementAtOrDefault(Step);
+
+        public WorkingMultiStep(string title)
+        {
+            Title = title;
+            Children = new List<WorkingStepBase>();
+            Step = 0;
+        }
+        public WorkingMultiStep WithChild(WorkingStepBase c)
+        {
+            c.Parent = this;
+            Children.Add(c);
+            return this;
+        }
+
+        public override void MarkDone()
+        {
+            Current.MarkDone();
+            if(Current.IsFinished)
+            {
+                Step++;
+            }
+        }
+
+        public override string Description => Current.Description;
+        public override int Delay => Current.Delay;
+        public override DateTime? ActuallyEndedAt => Current.ActuallyEndedAt;
+        public override int Duration => Current.Duration;
+        public override DateTime? StartedAt => Current.StartedAt;
+        public override DateTime? EndsAt => Current.EndsAt;
+        public override bool IsFinished => Step == Children.Count;
+        public override DateTime IdealStartAt => Current.IdealStartAt;
+
+        public override int FullLength { get
+            {
+                if (IsFinished) return 0;
+                return Children.Sum(x => x.FullLength);
+            } }
+
+        public override string GetDebuggerDisplay()
+        {
+            return $"[{Step}/{Children.Count}|{FullLength}] {(Current?.GetDebuggerDisplay() ?? "non")}";
+        }
+        public override void UpdateIdealStart(DateTime targetEndsAt)
+        {
+            if (IsFinished) return;
+            var theoretical = targetEndsAt.AddSeconds(FullLength * -1);
+            //Console.WriteLine($"== {Title} target {targetEndsAt}, theoretical start: {theoretical}");
+            for(int i = Children.Count - 1; i >= 0; i--)
+            {
+                WorkingStepBase child = Children[i];
+                child.UpdateIdealStart(targetEndsAt);
+                targetEndsAt = targetEndsAt.AddSeconds(child.FullLength * -1);
+            }
+            //Console.WriteLine($"== final end: {targetEndsAt}");
+
+            //IdealStartAt = targetEndsAt;
+        }
+
+        public override void MarkStarted()
+        {
+            Current.MarkStarted();
+            if(Step++ > 0)
+            {
+                Children[Step - 1].MarkDone();
+            }
+        }
     }
 }
