a55d2f05d147f9efcd8c1d842155dc166d65ce34
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2022-05-24 19:16:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ echo "!*/*" >> .gitignore
 echo "x64" >> .gitignore
 echo "x86" >> .gitignore
 git add .
+git config --global user.email "buildbot@github.com"
+git config --global user.name "Build Bot"
 git commit -a -m "Automatic forcepush of build"
 git remote add origin https://$2@github.com:CheAle14/bot-binary.git
 git push origin HEAD:$1 --force
