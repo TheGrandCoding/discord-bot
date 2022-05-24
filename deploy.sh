efd82bf582669c3c765c87c290df248cdbce09ab
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2022-05-24 19:14:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 echo Deploying to $1
 
-openssl aes-256-cbc -K $encrypted_871d352bed27_key -iv $encrypted_871d352bed27_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deploy_key.pem # this key should have push access
-ssh-add .travis/deploy_key.pem
+
 cd DiscordBot/bin/Release/net6.0/linux-arm
 git init
 echo "*.*" >> .gitignore
@@ -13,5 +10,5 @@ echo "x64" >> .gitignore
 echo "x86" >> .gitignore
 git add .
 git commit -a -m "Automatic forcepush of build"
-git remote add origin git@github.com:CheAle14/bot-binary.git
+git remote add origin https://$2@github.com:CheAle14/bot-binary.git
 git push origin HEAD:$1 --force
