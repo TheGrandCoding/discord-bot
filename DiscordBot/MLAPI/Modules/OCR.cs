b15ca5266d4e1e698f86bf739e83c8ea685c920a
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2023-01-31 20:48:15 +0000

    update cmd name

--- DiscordBot/MLAPI/Modules/OCR.cs
+++ DiscordBot/MLAPI/Modules/OCR.cs
@@ -26,7 +26,7 @@ namespace DiscordBot.MLAPI.Modules
         private string run_cmd(string cmd, string args)
         {
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = "python";
+            start.FileName = "python3";
             start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, args);
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
