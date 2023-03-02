9c923b246a6971d889105e4443ba65fcb6d827f6
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2023-03-02 17:20:59 +0000

    Fix

--- DiscordBot/MLAPI/Attributes/RequireOwner.cs
+++ DiscordBot/MLAPI/Attributes/RequireOwner.cs
@@ -14,7 +14,7 @@ namespace DiscordBot.MLAPI
 
         public override PreconditionResult Check(APIContext context, IServiceProvider services)
         {
-            return (context.User?.Id ?? 0) == (Program.AppInfo?.Owner?.Id ?? 1) 
+            return (context.User?.Connections?.DiscordId ?? "") == (Program.AppInfo?.Owner?.Id ?? 1).ToString() 
                 ? PreconditionResult.FromSuccess() 
                 : PreconditionResult.FromError("Only the bot's owner may access this");
         }
